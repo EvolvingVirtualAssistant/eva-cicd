bf6e491ba3ff284f0f8d4ee61be2b2aaca43c511
Pedro Sanches <Pedro-Sanches@users.noreply.github.com>
2021-07-08 13:59:42 +0100

    BUG: CASC config missing plugin and credentials management improvement (#2)
    * Fixes an issue where first jenkins docker setup on new machines would not be able to resolve the custom plugin reference in the casc_config jenkins.yaml file
    
    * Fixes issue with setting up admin password the first time the container ran. Removed add agent ssh cred and change password groovy scripts since we are now using casc jenkins.yaml file to config those credentials
    
    Co-authored-by: RuiSanches <rf.sanches@hotmail.com>

--- jenkins/setup-initial-configs-jenkins.sh
+++ jenkins/setup-initial-configs-jenkins.sh
@@ -9,8 +9,27 @@ response=''
 cookie_session_id=''
 crumb=''
 
+casc_config_bkp_file=$(echo "${CASC_JENKINS_CONFIG}".bkp)
+
 function log() {
     echo "[$(date "+%Y-%m-%d %H:%M:%S")][setup-script] $1"
+}
+
+function empty_casc_config_file() {
+    if [ -f "$casc_config_bkp_file" ]; then
+        log "No need to change ${CASC_JENKINS_CONFIG} file name so it doesn't get used in jenkins startup"
+    else
+        log "Renaming ${CASC_JENKINS_CONFIG} file so it doesn't get used in jenkins startup"
+        mv "${CASC_JENKINS_CONFIG}" "${casc_config_bkp_file}"
+        echo "" >>"${CASC_JENKINS_CONFIG}"
+    fi
+}
+
+function recover_casc_config_file() {
+    if [ -f "$casc_config_bkp_file" ]; then
+        log "Copying ${CASC_JENKINS_CONFIG} file so it starts gettign used in jenkins startup"
+        cp "${casc_config_bkp_file}" "${CASC_JENKINS_CONFIG}"
+    fi
 }
 
 function GET_request() {
@@ -70,6 +89,9 @@ function check_if_jenkins_user_already_has_password_set() {
         if [ -n "$isAuthorized" ]; then
             log "Jenkins user already has the right credentials"
             old_password=$new_password
+        elif [ -z "$old_password" ]; then
+            log "Could not load old_password properly, trying once again"
+            old_password=$(cat /var/jenkins_home/secrets/initialAdminPassword)
         fi
     fi
 }
@@ -102,7 +124,7 @@ function create_new_api_token() {
         exit 1
     fi
 
-    POST_create_jenkins_api_token "$username" "$new_password" "$crumb" "$cookie_session_id"
+    POST_create_jenkins_api_token "$username" "$old_password" "$crumb" "$cookie_session_id"
     get_jenkins_api_token "$response"
     if [ -z "$api_token" ] || [ "$api_token" == " " ]; then
         log "Could not extract api token from $response. Terminating."
@@ -112,6 +134,9 @@ function create_new_api_token() {
 
 log "Starting initial jenkins config setup..."
 
+# workaround to deal with the need of uploading the custom plugin first
+empty_casc_config_file
+
 wait_for_jenkins_start
 
 create_new_api_token
@@ -119,6 +144,9 @@ create_new_api_token
 # Uploads plugins
 download_jenkins_cli
 java -jar "${JENKINS_EVA_PLUGINS}"/jenkins-cli.jar -s http://localhost:8080 -auth "$username":"$api_token" install-plugin "file:///${JENKINS_EVA_PLUGINS}/pipeline-as-yaml-workflow-multi-branch-plugin.hpi" -restart
+
+# workaround to deal with the need of uploading the custom plugin first
+recover_casc_config_file
 
 log "Sleeping for 20 seconds waiting restart..."
 sleep 20
@@ -134,20 +162,9 @@ create_new_api_token
 
 log "Restarting jenkins config setup..."
 
-# Change user password if user still uses the default generated password
-if [ "$old_password" != "$new_password" ]; then
-    log "Trying to change jenkins user password..."
-    execute_script_in_jenkins "$username" "$api_token" "${JENKINS_EVA_SCRIPTS}/change-password.groovy"
-else
-    log "Skipping password change step"
-fi
-
 # Set locale options
 log "Trying to set locale options..."
 execute_script_in_jenkins "$username" "$api_token" "${JENKINS_EVA_SCRIPTS}/set-locale-options.groovy"
-
-# Add jenkins agent ssh credential - no longer needed since ssh key is already defined in jenkins.yaml configuration
-#execute_script_in_jenkins "$username" "$api_token" "${JENKINS_EVA_SCRIPTS}/add-agent-ssh-credentials.groovy"
 
 # Add jenkins agent
 log "Trying to add jenkins agent..."
