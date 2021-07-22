672f61c02efaf155cdc619b9c8ad328778a0f393
RuiSanches <rf.sanches@hotmail.com>
2021-07-22 20:29:32 +0100

    Add github api python lib

---
+++ triggers/gitbot/bot/services/github_service.py
@@ -0,0 +1,7 @@
+
+class GithubService():
+
+    def __init__(self):
+        # check this to interact with github https://pypi.org/project/ghapi/
+        # may be useful to get certain info such as labels on a PR
+        pass
