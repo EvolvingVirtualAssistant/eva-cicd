a13930fbec38a72c677524124144eaadcf6a2366
RuiSanches <rf.sanches@hotmail.com>
2021-08-07 19:20:04 +0100

    Testing printing location 2

--- triggers/jenkins/gitbot_ci.sh
+++ triggers/jenkins/gitbot_ci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 pwd
-#DOCKER_BUILDKIT=1 docker build -t eva-cicd-gitbot-debian --target continuous-integration ../gitbot/
+DOCKER_BUILDKIT=1 docker build -t eva-cicd-gitbot-debian --target continuous-integration triggers/gitbot/
