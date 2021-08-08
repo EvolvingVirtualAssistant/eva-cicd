55a511755eaf65c6e6c7b3e2040513bd1dc20099
RuiSanches <rf.sanches@hotmail.com>
2021-08-08 13:20:20 +0100

    Moves .env file to agent container and sets the values as env variables when running cicd on gitbot

--- triggers/jenkins/gitbot_ci.sh
+++ triggers/jenkins/gitbot_ci.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+cp /run/secrets/eva-cicd_env triggers/gitbot/eva-cicd_env
+
+# find and read the appropriate file that was copied to the agent container
+# set all of the read env vars only in the context of this file
+# add in the line below dynamically --env VAR_NAME, which will copy the env and pass it to the container
 DOCKER_BUILDKIT=1 docker build -t eva-cicd-gitbot-debian --target continuous-integration triggers/gitbot/
+
+# clean env file
+rm triggers/gitbot/eva-cicd_env
