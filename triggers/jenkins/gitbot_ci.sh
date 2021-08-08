d5d672102189a2df63ed41c5ad57460cb3620cb8
RuiSanches <rf.sanches@hotmail.com>
2021-08-08 20:01:49 +0100

    Fixes last details and removes comments from gitbot ci related files

--- triggers/jenkins/gitbot_ci.sh
+++ triggers/jenkins/gitbot_ci.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
+# use single quotes, escape single quotes, prefix every valid line with export
+# partially extracted from here https://stackoverflow.com/questions/15783701/which-characters-need-to-be-escaped-when-using-bash
 (cat ../../../../../../run/secrets/eva-cicd_env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" -e 's/^/export /') >triggers/gitbot/eva-cicd_env
-cat triggers/gitbot/eva-cicd_env
 
-# find and read the appropriate file that was copied to the agent container
-# set all of the read env vars only in the context of this file
-# add in the line below dynamically --env VAR_NAME, which will copy the env and pass it to the container
 DOCKER_BUILDKIT=1 docker build -t eva-cicd-gitbot-debian --target continuous-integration triggers/gitbot/
 
 # clean env file
